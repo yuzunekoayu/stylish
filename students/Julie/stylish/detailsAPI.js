009b81287b9ccf378b3a34e432dc69cee61b6146
Julie <sizumasa@hotmail.com>
2019-02-25 17:17:34 +0800

    variants & stock

--- students/Julie/stylish/detailsAPI.js
+++ students/Julie/stylish/detailsAPI.js
@@ -25,41 +25,75 @@ kwsk()
 
 // 選擇顏色尺寸，告訴顧客有無庫存
 function orderHelper(variants) {
-    console.log(variants);
-    
-    let order = { iro: '', size: '', amount: 0, userStock: 0 };
+    console.log(variants)
 
+    // 把客人選的顏色、尺寸、數量、庫存，存成一個物件（初始是 0 或 ""）
+    let order = { iro: '', size: '', amount: 0, userStock: 0 };
+    
     const iros = document.querySelectorAll('.iro');
+    const sizes = document.querySelectorAll('.sml');
+    const minus = document.querySelector("#minus");
+    const count = document.querySelector("#count");
+    const plus = document.querySelector("#plus");
+    const addToCart = document.querySelector("#addToCart");
+
+    // 監聽顏色被按
     iros.forEach( iro => {
         iro.addEventListener("click", memoOrderIro);
     });
+    // 顏色被按的 CallBack
     function memoOrderIro(e) {
         order.iro = e.currentTarget.style.backgroundColor;
         variants.forEach( variant => {
             if (RGBtoHex(variant.color_code) === order.iro && variant.size === order.size) {
                 order.userStock = variant.stock;
-                console.log('目前庫存Color', order.userStock);
+                tellIfSoldOut(order.userStock, addToCart);
             }
         })
     }
-    const sizes = document.querySelectorAll('.sml');
+
+    // 監聽尺寸被按
     sizes.forEach( size => {
         size.addEventListener("click", memoOrderSize);
     });
+    // 尺寸被按的 CallBack
     function memoOrderSize(e) {
         order.size = e.currentTarget.innerText;
         variants.forEach( variant => {
             if (RGBtoHex(variant.color_code) === order.iro && variant.size === order.size) {
                 order.userStock = variant.stock;
-                console.log('目前庫存Size', order.userStock);
+                tellIfSoldOut(order.userStock, addToCart);
             }
         })
     }
 
+    // 監聽減號被按
+    minus.addEventListener("click", () => {
+        if (count.value <= order.userStock && count.value > 0) {
+            addToCart.innerText = "加入購物車";
+            count.value --;
+        }
+    });
+    
+    // 監聽加號被按
+    plus.addEventListener("click", () => {
+        if (count.value === 0 || count.value < order.userStock) {
+            count.value ++;
+        } else if (count.value = order.userStock) {
+            addToCart.innerText = "加入購物車（已達最大庫存）";
+        }
+    });
 
-
-    order.amount = document.querySelector("#count").value;
-    let addToCart = document.querySelector("#addToCart");
+    // 監聽「加入購物車」按鈕被按
+    addToCart.addEventListener("click", () => {
+        if (order.iro === "" || order.size === "") {
+            addToCart.disabled = true;
+            addToCart.innerText = "請選擇顏色及尺寸";
+        } else if  (order.iro !== "" && order.size !== "" && count.amount > 0) {
+            addToCart.disabled = false;
+            addToCart.innerText = "加入購物車";
+        }
+    });    
     
     
         
--- students/Julie/stylish/product.js
+++ students/Julie/stylish/product.js
@@ -2,18 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
   
   toggleMutiple(document.querySelectorAll('.item'), "activeTab");
 
-  const minus = document.querySelector("#minus");
-  const count = document.querySelector("#count");
-  const plus = document.querySelector("#plus");
-
-  minus.addEventListener("click", () => {
-    if (count.value > 0) {
-      count.value--;
-    }
-  });
-
-  plus.addEventListener("click", () => {
-    count.value++;
-  });
-
 });
+
+const tellIfSoldOut = (num, btn) => {
+  if (num === 0) {
+    btn.disabled = true;
+    btn.innerText = "缺貨中";
+    btn.classList.add("failToCart");
+  } else {
+    btn.disabled = false;
+    btn.innerText = "加入購物車";
+    btn.classList.remove("failToCart");
+  }
+}
+
