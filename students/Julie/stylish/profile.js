704c50447cedf0ed5a2bb6518f732dd1aea20ecb
Julie <sizumasa@hotmail.com>
2019-03-04 21:49:38 +0800

    gh-page commit

--- students/Julie/stylish/profile.js
+++ students/Julie/stylish/profile.js
@@ -1,80 +1,80 @@
-// Load the SDK asynchronously
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+document.addEventListener("DOMContentLoaded", () => {
 
-// 初始化 FB SDK 套件
-window.fbAsyncInit = function() {
-    FB.init({
-        appId      : '541771016312048',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v3.2'
-    });
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
 
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-        console.log(response);
-    });
-};
+    // 初始化 FB SDK 套件
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '541771016312048',
+            cookie     : true,
+            xfbml      : true,
+            version    : 'v3.2'
+        });
 
-// This is called with the results from from FB.getLoginStatus().
-function statusChangeCallback(response) {
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+            console.log(response);
+        });
+    };
 
-    const profileNotIn = document.querySelector('#profileNotIn');
-    const profileCard = document.querySelector('#profileCard');
-    
-    console.log('statusChangeCallback');
-    
-    if (response.status === 'connected') {
-        // Logged into your app and Facebook.
-        profileNotIn.style.display = "none";
-        profileCard.style.display = "flex";
-        console.log("YOOO");
-        testAPI();
-    } else {
-        // The person is not logged into your app or we are unable to tell.
-        profileNotIn.style.display = "flex";
-        // profileCard.style.display = "none";
-        console.log("NOT IN")
-    }
-}
+    // This is called with the results from from FB.getLoginStatus().
+    function statusChangeCallback(response) {
 
-// Here we run a very simple test of the Graph API after login is
-// successful.  See statusChangeCallback() for when this call is made.
-function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
-    FB.api("/me?fields=id, name, email", function(response) {
-        console.log(response);
-
-        const memberIcon = document.querySelector('.member');
-        const mobileMemberIcon = document.querySelector('.smIcon');
+        const profileNotIn = document.querySelector('#profileNotIn');
+        const profileCard = document.querySelector('#profileCard');
         
-        memberIcon.style.background = `url(https://graph.facebook.com/${response.id}/picture?type=small) no-repeat`;
-        mobileMemberIcon.style.background = `url(https://graph.facebook.com/${response.id}/picture?type=small) no-repeat`;
-
-        if ( window.location.href.indexOf("profile") > -1) {
-            const userIcon = document.querySelector('#profileIcon');
-            let userIconImg = `https://graph.facebook.com/${response.id}/picture?type=large`;
-            const userName = document.querySelector('#profileName');
-            const userEmail = document.querySelector('#profileEmail');
-
-            userIcon.style.background = `url(${userIconImg}) no-repeat`;
-            userName.textContent = response.name;
-            userEmail.textContent = response.email;
+        if (response.status === 'connected') {
+            // Logged into your app and Facebook.
+            profileNotIn.style.display = "none";
+            profileCard.style.display = "flex";
+            console.log("YOOO");
+            testAPI();
+        } else {
+            // The person is not logged into your app or we are unable to tell.
+            profileNotIn.style.display = "flex";
+            console.log("NOT IN")
         }
-    });
-}
+    }
 
-// This function is called when someone finishes with the Login
-// Button.  See the onlogin handler attached to it in the sample
-// code below.
-function checkLoginState() {
-    FB.login(function(response) {
-        statusChangeCallback(response);
-    }, {scope:"email"});
-}
+    // Here we run a very simple test of the Graph API after login is
+    // successful.  See statusChangeCallback() for when this call is made.
+    function testAPI() {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api("/me?fields=id, name, email", function(response) {
+            console.log(response);
+
+            const memberIcon = document.querySelector('.member');
+            const mobileMemberIcon = document.querySelector('.smIcon');
+            
+            memberIcon.style.background = `url(https://graph.facebook.com/${response.id}/picture?type=small) no-repeat`;
+            mobileMemberIcon.style.background = `url(https://graph.facebook.com/${response.id}/picture?type=small) no-repeat`;
+
+            if ( window.location.href.indexOf("profile") > -1) {
+                const userIcon = document.querySelector('#profileIcon');
+                let userIconImg = `https://graph.facebook.com/${response.id}/picture?type=large`;
+                const userName = document.querySelector('#profileName');
+                const userEmail = document.querySelector('#profileEmail');
+
+                userIcon.style.background = `url(${userIconImg}) no-repeat`;
+                userName.textContent = response.name;
+                userEmail.textContent = response.email;
+            }
+        });
+    }
+
+    // This function is called when someone finishes with the Login
+    // Button.  See the onlogin handler attached to it in the sample
+    // code below.
+    function checkLoginState() {
+        FB.login(function(response) {
+            statusChangeCallback(response);
+        }, {scope:"email"});
+    }
+});
