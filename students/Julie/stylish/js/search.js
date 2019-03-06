119962a3ad8c934f3e5558a77335fa6f86bed57f
Julie <sizumasa@hotmail.com>
2019-03-06 22:23:24 +0800

    css issue on safari and firefox

--- students/Julie/stylish/js/search.js
+++ students/Julie/stylish/js/search.js
@@ -24,12 +24,12 @@ const feature = document.querySelector("#feature");
 const searchBar = document.querySelector("#searchBar");
 
 function magnify() {
-  if (mobileSubmit.style.backgroundImage === 'url("./img/close.png") no-repeat') {
-    mobileSubmit.style.backgroundImage = 'url("./img/search.png") no-repeat';
+  if (mobileSubmit.style.backgroundImage === 'url("./img/close.png")') {
+    mobileSubmit.style.backgroundImage = 'url("./img/search.png")';
     feature.classList.remove('featureGrow');
     searchBar.classList.remove('searchBarGrow');
   } else {
-    mobileSubmit.style.backgroundImage = 'url("./img/close.png") no-repeat';
+    mobileSubmit.style.backgroundImage = 'url("./img/close.png")';
     feature.classList.add('featureGrow');
     searchBar.classList.add('searchBarGrow');
   }
